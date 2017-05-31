93519ed17e17e0e143bad25aeb7e6099633dc914
kburiev <khasanburiev@gmail.com>
2017-05-31 17:20:57 +0200

    correction in new user

--- bcproject/config/routes.rb
+++ bcproject/config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
-
-    devise_for :users
-    scope "/:locale", locale: /#{I18n.available_locales.join("|")}/ do
-        resources :posts
-            root to: redirect("/%{locale}/posts", status: 302)
-              end
-                root to: redirect("/#{I18n.default_locale}", status: 302), as: :redirected_root
-                  get "/*path", to: redirect("/#{I18n.default_locale}/%{path}", status: 302), constraints: {path: /(?!(#{I18n.available_locales.join("|")})\/).*/}, format: false
-                    end
+#root to: 'posts#index'
+devise_for :users
+#resources :users_path, :controller => 'users'
+scope "/:locale", locale: /#{I18n.available_locales.join("|")}/ do
+resources :posts
+root to: redirect("/%{locale}/posts", status: 302)
+end
+root to: redirect("/#{I18n.default_locale}", status: 302), as: :redirected_root
+get "/*path", to: redirect("/#{I18n.default_locale}/%{path}", status: 302), constraints: {path: /(?!(#{I18n.available_locales.join("|")})\/).*/}, format: false
+end
 
